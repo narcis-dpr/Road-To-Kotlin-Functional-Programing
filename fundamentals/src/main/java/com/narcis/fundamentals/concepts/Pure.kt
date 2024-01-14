0af826e64ec2181144713dddd10c05b4b3a6ce8b
narges-dpr <nargesdeypir@gmail.com>
2024-01-14 22:08:02 +0330

    functional fundamentals -- concepts: pure

--- fundamentals/src/main/java/com/narcis/fundamentals/concepts/Pure.kt
+++ fundamentals/src/main/java/com/narcis/fundamentals/concepts/Pure.kt
@@ -2,9 +2,9 @@ package com.narcis.fundamentals.concepts
 
 import com.narcis.fundamentals.basics.Fun
 
-fun twice(x: Int): Int = x*2
+fun twice(x: Int): Int = x*2 //pure
 
-fun twiceAndLog(x: Int): Int {
+fun twiceAndLog(x: Int): Int { // not pure
     val result = x*2
     println("2 * $x = $result")
     return result
