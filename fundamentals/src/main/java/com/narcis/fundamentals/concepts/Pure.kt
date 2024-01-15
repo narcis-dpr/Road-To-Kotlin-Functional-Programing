7b1715c2c4a069cd4548a6cd456d2247097bdc38
narges-dpr <nargesdeypir@gmail.com>
2024-01-15 22:29:12 +0330

    functional fundamentals -- concepts: pure and impure

--- fundamentals/src/main/java/com/narcis/fundamentals/concepts/Pure.kt
+++ fundamentals/src/main/java/com/narcis/fundamentals/concepts/Pure.kt
@@ -1,6 +1,7 @@
 package com.narcis.fundamentals.concepts
 
 import com.narcis.fundamentals.basics.Fun
+import kotlin.random.Random
 
 fun twice(x: Int): Int = x*2 //pure
 
@@ -10,6 +11,23 @@ fun twiceAndLog(x: Int): Int { // not pure
     return result
 }
 
+// not pure :
+var count = 1
+fun inc(x: Int): Int = ++count + x
+// not pure :
+fun randomInc(a: Int): Int = a + Random.nextInt()
+// pure :
+var count2 = 0
+fun inc2(x: Int): Int = x + count2 + 1
+// all pure :
+fun negate(x: Int) = -x
+fun identity(x: Int) = x
+fun abs(x: Int) = if (x<0) negate(x) else identity(x) // compose from other pure functions
+// not pure :
+fun countedAbs(x: Int): Int{
+    count2++
+    return abs(x)
+}
 fun main() {
     var f: Fun<Int, Int> = ::twice
     println("Executing twice(10)")
