599be051df32682b3d96a4388b4c9c6e6f9d3cbe
n.dipir <nargesdeypir@gmail.com>
2024-02-12 14:27:23 +0330

    higher order functions : functional interface vs typeAlias

--- Higher Order Function/src/main/java/com/narcis/higherOrder/higherOrder/FunctionalVsLambda.kt
+++ Higher Order Function/src/main/java/com/narcis/higherOrder/higherOrder/FunctionalVsLambda.kt
@@ -4,7 +4,22 @@ typealias SinglePredicate<T> = (T) -> Boolean
 
 fun <T> SinglePredicate<T>.whoAmI() = println("Im a typealias")
 
+
+fun interface ISinglePredicate<T> {
+    fun accept(value: T): Boolean
+
+    fun other() {
+        println(" I can have other methods")
+    }
+}
+fun <T> ISinglePredicate<T>.whoAmI() =
+    println("Im a functional interface")
 fun main() {
     val isEven = {number : Int -> number % 2 == 0}
     isEven.whoAmI()
+
+    val isEvenFI = ISinglePredicate<Int> {number -> number%2 == 0}
+
+    isEvenFI.whoAmI()
+    isEvenFI.other()
 }
