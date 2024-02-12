40d0c462f066235201ab090065a39c3aeddcd347
n.dipir <nargesdeypir@gmail.com>
2024-02-12 09:28:27 +0330

    higher order functions : Generic bubble sort

--- Higher Order Function/src/main/java/com/narcis/higherOrder/higherOrder/Strategy.kt
+++ Higher Order Function/src/main/java/com/narcis/higherOrder/higherOrder/Strategy.kt
@@ -1,25 +1,45 @@
 package com.narcis.higherOrder.higherOrder
 
 // classical bubble sort
-fun bubbleSort(values: IntArray) {
+fun bubbleSortNoneGeneric(values: IntArray) {
     for (i in values.size -1 downTo 0) {
         for (j in 0 until i) {
             if (values[j] > values[j + 1]) {
-                swap(values, j, j + 1)
+                swapNoneGeneric(values, j, j + 1)
+            }
+        }
+    }
+}
+
+fun <T> bubbleSort(
+    values: Array<T>,
+    isLarger: (T, T) -> Boolean
+) {
+    for (i in values.size-1 downTo 0) {
+        for (j in 0 until i) {
+            if (isLarger(values[j], values[j+1])) {
+                swap(values, j, j+1)
             }
         }
     }
 }
 fun main() {
     val array = intArrayOf(10, 5, 2, 7, 8, 3)
-    bubbleSort(array)
+    bubbleSortNoneGeneric(array)
     array.printAll()
 }
 
 
 
 
-fun swap(values: IntArray, i: Int, j: Int) {
+fun swapNoneGeneric(values: IntArray, i: Int, j: Int) {
+    if (values[i] != values[j]) {
+        val tmp = values[i]
+        values[i] = values[j]
+        values[j] = tmp
+    }
+}
+fun <T> swap(values: Array<T>, i: Int, j: Int) {
     if (values[i] != values[j]) {
         val tmp = values[i]
         values[i] = values[j]
