0c94961e66aa875e0f4ef588485a25789ab20f08
n.dipir <nargesdeypir@gmail.com>
2024-02-14 15:57:16 +0330

    higher order functions : inline

--- Higher Order Function/src/main/java/com/narcis/higherOrder/higherOrder/Inline.kt
+++ Higher Order Function/src/main/java/com/narcis/higherOrder/higherOrder/Inline.kt
@@ -3,4 +3,16 @@ package com.narcis.higherOrder.higherOrder
 inline fun executor(fn: () -> Unit) {fn()}
 fun main() {
     executor { println("Hello World!") }
+
+    executor {
+        var count = 0
+        while (true) {
+            count++
+            if (count > 5) {
+                return  // if you dont inline executor, the compiler complains about not knowing
+                        // from what context it should actually return
+            }
+        }
+    }
+    println("The End!")
 }
