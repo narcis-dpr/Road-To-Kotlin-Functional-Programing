5d650c5acab93941c9e9053649131d95728aaa77
n.dipir <nargesdeypir@gmail.com>
2024-02-17 10:19:34 +0330

    Immutability and recursion: wrong immutable

--- Immuttability and Recursion/src/main/java/com/narcis/recursion/immutability/Immutable.kt
+++ Immuttability and Recursion/src/main/java/com/narcis/recursion/immutability/Immutable.kt
@@ -1,6 +1,21 @@
 package com.narcis.recursion.immutability
 
+import java.util.Date
+
 data class User( // this is an immutable class because every property is val
     val id: Int, // string and Int are also immutable
     val username: String
 )
+
+data class WrongImmutableUser(
+    val id: Int,
+    val username: String,
+    val dob: java.util.Date = Date() // because of this the class isn't fully immutable
+)
+
+fun main() {
+    val w = WrongImmutableUser(1, "maxcarli")
+    println(w)
+    w.dob.time = 1000L // access dob and changing it
+    println(w)
+}
