ecd9ae96783c6da9d7cb83074dce3cf04f06ab67
n.dipir <nargesdeypir@gmail.com>
2024-02-18 08:57:16 +0330

    Immutability and recursion: create the same instance multiple times

---
+++ Immuttability and Recursion/src/main/java/com/narcis/recursion/immutability/Reuse.kt
@@ -0,0 +1,16 @@
+package com.narcis.recursion.immutability
+
+data class Key(
+    val id: Int
+)
+fun main() {
+    val key1 = Key(1)
+    val key2 = Key(1)
+    val myMap = mutableMapOf<Key, String>()
+    myMap[key1] = "First"
+    println("Value for $key1 is ${myMap[key1]}")
+    println("The Map is $myMap")
+    myMap[key2] = "Second"
+    println("Value for $key2 is ${myMap[key2]}")
+    println("The Map is $myMap")
+}
