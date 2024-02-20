afa426be0d635a87e52169cefa24f7ff9403f6db
n.dipir <nargesdeypir@gmail.com>
2024-02-20 11:52:45 +0330

    Immutability and recursion: mutability in functional programming

---
+++ Immuttability and Recursion/src/main/java/com/narcis/recursion/recursion/FPImutability.kt
@@ -0,0 +1,22 @@
+package com.narcis.recursion.recursion
+
+fun main() {
+    // imperative approach
+    var total = 0
+    val list = listOf(1, 5, 10, 12, 34, 55, 80, 23, 35, 12, 80)
+    for (i in list.indices) {
+        if (list[i]%5 == 0) {
+            total += list[i]
+        }
+    }
+    println("Total: $total")
+
+    // declarative approach : in this case you dont see anything mutable, you dont see any index to update for iterating over the list
+    val multipleOf5 = {value: Int -> value%5 == 0}
+    val totalD = listOf(1, 5, 10, 12, 34, 55, 80, 23, 35, 12, 80)
+        .filter(multipleOf5)
+        .sum()
+
+    println("Total: $total")
+
+}
