dc3905cf7a946dfe708379c54fe8e09c0ff4ea6d
n.dipir <nargesdeypir@gmail.com>
2024-02-26 17:48:21 +0330

    functional data structure -- Object Oriented List

--- Functional Data Structure/src/main/kotlin/com/example/db/ObjectOrientedList.kt
+++ Functional Data Structure/src/main/kotlin/com/example/db/ObjectOrientedList.kt
@@ -1,2 +1,11 @@
 package com.example.db
 
+data class Node<T>(
+    val value: T,
+    val next: Node<T>? = null
+)
+
+fun main() {
+    val emptyList: Node<*>? = null
+    val singleValueList = Node(1)
+}
