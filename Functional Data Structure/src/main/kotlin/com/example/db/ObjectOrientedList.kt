130dd14c96b192b00657854f2f93c8caa0f259eb
n.dipir <nargesdeypir@gmail.com>
2024-02-27 16:20:15 +0330

    functional data structure -- link list

--- Functional Data Structure/src/main/kotlin/com/example/db/ObjectOrientedList.kt
+++ Functional Data Structure/src/main/kotlin/com/example/db/ObjectOrientedList.kt
@@ -8,4 +8,7 @@ data class Node<T>(
 fun main() {
     val emptyList: Node<*>? = null
     val singleValueList = Node(1)
+    val modifiedSingleValue = Node(1, emptyList as Node<Int>)
+    val singleValueWithNoRelation = Node(1, null)
+    val twoValuesList = Node(2, Node(1, null))
 }
