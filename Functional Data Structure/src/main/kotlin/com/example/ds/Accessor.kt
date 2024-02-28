2270bb9ffa3ae7c0788a7a1bc29abeead555ba1e
n.dipir <nargesdeypir@gmail.com>
2024-02-28 14:05:02 +0330

    functional data structure -- add head and tail accessor

--- Functional Data Structure/src/main/kotlin/com/example/ds/Accessor.kt
+++ Functional Data Structure/src/main/kotlin/com/example/ds/Accessor.kt
@@ -6,10 +6,28 @@ matching patterns for comparing
  */
 fun <T> FList<T>.size(): Int = match(
     whenNil = { 0 },
-    whenCons = {head, tail -> 1 + tail.size() }
+    whenCons = { head, tail -> 1 + tail.size() }
 )
+
+/*
+null if the receiver is Nil, head if the receiver is FCons<T> :
+ */
+fun <T> FList<T>.head(): T? = match(
+    whenNil = { null },
+    whenCons = { head, tail -> head }
+)
+
+fun <T> FList<T>.tail(): FList<T>? = match(
+    whenNil = { null },
+    whenCons = { head, tail -> tail }
+)
+
 fun main() {
     println(FList.empty<Int>().size())
+    println(FList.of(1).size())
+    println(FList.of(1, 2, 3).size())
+
+    println(FList.empty<Int>().head())
     println(FList.of(1).size())
     println(FList.of(1, 2, 3).size())
 }
