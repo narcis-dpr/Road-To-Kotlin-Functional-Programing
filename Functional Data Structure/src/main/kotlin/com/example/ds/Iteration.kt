b1b491df8f2589512e89527a5cc372bb1348f265
n.dipir <nargesdeypir@gmail.com>
2024-02-28 16:07:53 +0330

    functional data structure -- add iteration to the list

---
+++ Functional Data Structure/src/main/kotlin/com/example/ds/Iteration.kt
@@ -0,0 +1,29 @@
+package com.example.ds
+
+fun <T> FList<T>.forEach(fn: (T) -> Unit): Unit = match(
+    whenNil = {},
+    whenCons = { head, tail ->
+        fn(head)
+        tail.forEach(fn)
+    }
+)
+fun <T> FList<T>.forEachIndexed(fn: (Int, T) -> Unit) {
+    fun FList<T>.loop(i: Int = 0): Unit = match(
+        whenNil = {},
+        whenCons = {head, tail ->
+            fn(i, head)
+            tail.loop(i + 1)
+        }
+    )
+}
+fun main() {
+    listOf(1, 2, 3).forEach {
+        println(it)
+    }
+    FList.of(1, 2, 3).forEach {
+        println(it)
+    }
+    FList.of("a", "b", "c").forEachIndexed {index, item ->
+        println("$index $item")
+    }
+}
