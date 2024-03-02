e4e1be0e81cb83d98fb941ca7e3e0d6e28f5d31e
n.dipir <nargesdeypir@gmail.com>
2024-03-02 12:30:28 +0330

    composition -- add curry to composition

--- Composition/src/main/java/com/narcis/composition/Curry.kt
+++ Composition/src/main/java/com/narcis/composition/Curry.kt
@@ -1,15 +1,32 @@
 package com.narcis.composition
 
+
 fun main() {
-    val double = { a: Int -> a * 2}
+    val double = { a: Int -> a * 2 }
     val square = { a: Int -> a * a }
-    val sum = { a: Int, b: Int -> a+b }
+    val sum = { a: Int, b: Int -> a + b }
     val stringify = Int::toString // returns the string representation of the int input value
 
     // a practice of currying :
     val addThree = sum(3)
     val result = addThree(4)
     println(result)
+
+    val curriedSum = sum.curry()
+    val addThree2 = curriedSum(3)
+    val result2 = addThree2(4)
+    println(result2)
+
 }
 
-fun sum(a: Int): (Int) -> Int = {b: Int -> a+b}
+fun sum(a: Int): (Int) -> Int = { b: Int -> a + b }
+
+// make curry generic :
+typealias Fun2<A, B, C> = (A, B) -> C // type of a function that gets 2 var and returns one
+
+fun <A, B, C> Fun2<A, B, C>.curry(): (A) -> (B) -> C = { a: A ->
+
+    { b: B ->
+        this(a, b)
+    }
+}
