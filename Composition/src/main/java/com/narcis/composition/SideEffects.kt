7ad9b5e11a11b350d19748fae9ae14cdebfb06d4
n.dipir <nargesdeypir@gmail.com>
2024-03-04 11:32:00 +0330

    composition -- pure vs side effect comparison in composition

--- Composition/src/main/java/com/narcis/composition/SideEffects.kt
+++ Composition/src/main/java/com/narcis/composition/SideEffects.kt
@@ -13,8 +13,20 @@ fun main() {
     functionWithEffect(5) pipe ::println
 
     listOf(1, 2, 3)
-        .map (::pureFunction) pipe ::println
+        .map(::pureFunction) pipe ::println
+
+
+    // this two has the same result and are equal :
+    listOf(1, 2, 3)
+        .map(::pureFunction).map(::pureFunction) pipe ::println
+
+    listOf(1, 2, 3)
+        .map(::pureFunction compose ::pureFunction) pipe ::println
+
+    // this two are not equal (because of side effect) this means functions with side effect dont compose! :
+    listOf(1, 2, 3).map(::functionWithEffect).map(::functionWithEffect) pipe ::println
 }
+
 // adding side effect :
 fun functionWithEffect(x: Int): Int { // returns the same result as pure function but with side effect
     val result = x * x - 1
