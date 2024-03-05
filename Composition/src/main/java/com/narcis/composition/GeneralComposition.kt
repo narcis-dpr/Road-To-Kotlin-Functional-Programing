d6a6823b8c92996888fe891e4ea5c17fdb138e33
n.dipir <nargesdeypir@gmail.com>
2024-03-05 11:07:05 +0330

    general composition -- compose with infix

--- Composition/src/main/java/com/narcis/composition/GeneralComposition.kt
+++ Composition/src/main/java/com/narcis/composition/GeneralComposition.kt
@@ -1,3 +1,39 @@
 package com.narcis.composition
 
-typealias opt<A, B> = (A) -> B?
+import java.lang.NumberFormatException
+
+typealias Opt<A, B> = (A) -> B?
+
+infix fun <A, B, C> Opt<A, B>.compose(
+    g: Opt<B, C>
+): Opt<A, C> = { a: A ->
+    val b = this(a)
+    if (b != null) {
+        g(b)
+    } else {
+        null
+    }
+}
+
+fun main() {
+    val strToInt = { str: String ->
+        try {
+            str.toInt()
+        } catch (nfe: NumberFormatException) {
+            null
+        }
+    }
+
+    val findUser = { id: Int ->
+        if (id == 3) User(3, "Max") else null
+    }
+    val strToUser = strToInt compose findUser
+    strToUser("a") pipe ::println
+    strToUser("2") pipe ::println
+    strToUser("3") pipe ::println
+}
+
+data class User(
+    val id: Int,
+    val name: String
+)
