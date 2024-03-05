4be9f120532f2811ea09ab75fa9062dc3edd3f43
n.dipir <nargesdeypir@gmail.com>
2024-03-05 16:53:49 +0330

    general composition -- remove side effects

--- Composition/src/main/java/com/narcis/composition/Mutation.kt
+++ Composition/src/main/java/com/narcis/composition/Mutation.kt
@@ -17,3 +17,15 @@ fun doubleWithMutationEffect(x: Int): Int {
     counter.count /= 2
     return result
 }
+// the process of purifying :
+typealias Updater<T> = (T) -> T // the abstraction of any function that maps objects in another object of the same type
+
+fun squareWithEffect(x: Int): Pair<Int, Updater<MutableCounter>> {
+    val result = x * x
+    return result to { counter -> counter.count *= 10; counter}
+}
+
+fun doubleWithEffect(x: Int): Pair<Int, Updater<MutableCounter>> {
+    val result = x * 2
+    return result to { counter -> counter.count /2; counter}
+}
