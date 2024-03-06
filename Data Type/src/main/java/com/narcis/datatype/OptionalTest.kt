eb77b12d2c70a0c307c0b35e640e6c08a68af0f5
n.dipir <nargesdeypir@gmail.com>
2024-03-06 12:52:49 +0330

    data type -- testing Optional<T> data type (beautiful)

---
+++ Data Type/src/main/java/com/narcis/datatype/OptionalTest.kt
@@ -0,0 +1,23 @@
+package com.narcis.datatype
+
+import java.lang.NumberFormatException
+
+fun strToInt(value: String): Optional<Int> = // since this operation can fail, the neuter type is an optional<Int>
+    try {
+        Optional.lift(value.toInt())
+    } catch (nfe: NumberFormatException) {
+        Optional.empty()
+    }
+
+fun double(value: Int): Int = value * 2
+
+fun main() {
+    val res = strToInt("10")
+    when(res) {
+        is Some<Int> -> {
+            val res2 = double(res.value)
+            println("Result is $res2")
+        }
+        is None -> println("Error!")
+    }
+}
