b9c5c1fda006ef218ab94ea8844cf12759b50b19
narcis <nargesdeypir.com>
2024-03-11 19:06:13 +0100

    data type --- add right and left map

--- Data Type/src/main/java/com/narcis/datatype/Either.kt
+++ Data Type/src/main/java/com/narcis/datatype/Either.kt
@@ -15,3 +15,17 @@ sealed class Either<out A, out B> {
 
 data class Left<A>(val left: A): Either<A, Nothing>()
 data class Right<B>(val right: B): Either<Nothing, B>()
+
+fun <A, B, C> Either<A, B>.leftMap(
+    fl: (A) -> C
+): Either<C, B> = when(this) {
+    is Left<A> -> Either.left(fl(left))
+    is Right<B> -> this // return the receiver itself
+}
+
+fun <A, B, D> Either<A, B>.rightMap(
+    fr: (B) -> D
+): Either<A, D> = when(this) {
+    is Right<B> -> Either.right(fr(right))
+    is Left<A> -> this
+}
