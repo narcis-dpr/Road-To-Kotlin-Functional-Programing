426eefe4529cc9a6f3ddfd713ddebba8266b6de4
n.dipir <nargesdeypir@gmail.com>
2024-03-12 11:48:39 +0330

    algebraic data type -- basic structure: add unit triage

--- Algebraic Data Types/src/main/java/com/narcis/algbdatatype/Sctruct.kt
+++ Algebraic Data Types/src/main/java/com/narcis/algbdatatype/Sctruct.kt
@@ -9,3 +9,5 @@ typealias BoolTriage = Pair<Boolean, Triage>
 
 typealias UnitTriage = Pair<Unit, Triage> // unit is equivalent to the value 1 when you multiply by it
 
+typealias TriageUnit = Pair<Triage, Unit>
+
