fb7943da9b4d21d6be6cde1027c337cf9ff0e7b8
n.dipir <nargesdeypir@gmail.com>
2024-03-13 14:46:53 +0330

    algebraic data type -- type safe callback usage

--- Algebraic Data Types/src/main/java/com/narcis/algbdatatype/TypeSafeCallback.kt
+++ Algebraic Data Types/src/main/java/com/narcis/algbdatatype/TypeSafeCallback.kt
@@ -2,3 +2,14 @@ package com.narcis.algbdatatype
 
 typealias Callback<Data, Result, Error> = (Data, Result?, Error?) -> Unit
 
+class Response
+class Info
+class ErrorInfo
+
+fun runAsync(callback: Callback<Response, Info, ErrorInfo>) {
+    // in case of success :
+    callback(Response(), Info(), null)
+
+    // in case of error :
+    callback(Response(), null, ErrorInfo())
+}
