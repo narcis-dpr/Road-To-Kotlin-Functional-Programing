e2e6a3ebd14a064425e087f671fd2b6af1f10f73
n.dipir <nargesdeypir@gmail.com>
2024-03-16 11:42:32 +0330

    algebraic data type -- list type

--- Algebraic Data Types/src/main/java/com/narcis/algbdatatype/List.kt
+++ Algebraic Data Types/src/main/java/com/narcis/algbdatatype/List.kt
@@ -2,9 +2,9 @@ package com.narcis.algbdatatype
 
 sealed class NaturalNumber
 
-object Zero: NaturalNumber()
+object Zero : NaturalNumber()
 
-data class Successor(val prev: NaturalNumber): NaturalNumber()
+data class Successor(val prev: NaturalNumber) : NaturalNumber()
 
 val ZERO = Zero
 
@@ -13,3 +13,17 @@ val ONE = Successor(Zero)
 val TWO = Successor(Successor(Zero))
 
 val THREE = Successor(Successor(Successor(Zero)))
+
+sealed interface FList<out A>
+object Nil : FList<Nothing>
+data class FCons<A>(
+    val head: A,
+    val tail: FList<A> = Nil
+) : FList<A>
+
+
+fun main() {
+    // test list :
+    FCons(1, FCons(2, FCons(3, FCons(4, FCons(5, Nil)))))
+
+}
