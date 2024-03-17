a45e125167f1b6f65c68f9ac4eab9e2f5d50956c
n.dipir <nargesdeypir@gmail.com>
2024-03-17 16:06:48 +0330

    monoids and semigroups -- more property testing the test

--- Monoids/src/test/java/com/narcis/monoids/PropertyTestTest.kt
+++ Monoids/src/test/java/com/narcis/monoids/PropertyTestTest.kt
@@ -15,3 +15,34 @@ fun `sum test using random values 100 times`() {
 
     }
 }
+
+// a + b = b + a
+@Test
+fun `test sum is commutative`(){
+    100.times {
+        val firstValue = Random.nextInt()
+        val secondValue = Random.nextInt()
+        val result1 = sum(firstValue, secondValue)
+        val result2 = sum(secondValue, firstValue)
+        Truth.assertThat(result1).isEqualTo(result2)
+    }
+
+    @Test
+    fun `test addition is not multiplication`(){
+        val randomValue = Random.nextInt()
+        val result1 = sum(sum(randomValue, 1), 1)
+        val result2 = sum(randomValue, 2)
+        Truth.assertThat(result1).isEqualTo(result2)
+    }
+
+    @Test
+    fun `test using unit value for addition`() {
+        100.times {
+            val randomValue = Random.nextInt()
+            val result1 = sum(randomValue, 0)
+            val expected = randomValue
+            Truth.assertThat(result1).isEqualTo(expected)
+        }
+    }
+
+}
