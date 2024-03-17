3933288d4d5e7ac3335a993dd1b43612c9fa867a
n.dipir <nargesdeypir@gmail.com>
2024-03-17 10:42:08 +0330

    monoids and semigroups -- add test dependency

--- Monoids/build.gradle.kts
+++ Monoids/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("org.testng:testng:6.9.6")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
+    implementation("com.google.truth:truth:1.1.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
