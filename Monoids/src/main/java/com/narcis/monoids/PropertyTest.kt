47f218c816a26abf56707c86366d021d13264e84
n.dipir <nargesdeypir@gmail.com>
2024-03-17 12:37:16 +0330

    monoids and semigroups -- property test

--- Monoids/src/main/java/com/narcis/monoids/PropertyTest.kt
+++ Monoids/src/main/java/com/narcis/monoids/PropertyTest.kt
@@ -2,6 +2,7 @@ package com.narcis.monoids
 
 import com.google.common.truth.Truth
 import org.junit.Test
+import kotlin.random.Random
 
 fun sum(a: Int, b: Int): Int = a + b
 
@@ -11,5 +12,13 @@ class PropertyTest {
         Truth.assertThat(sum(2,3)).isEqualTo(5)
         Truth.assertThat(sum(2, 5)).isEqualTo(7)
         Truth.assertThat(sum(-2, 5)).isEqualTo(3)
+    }
+
+    @Test
+    fun sum_test_using_random_values() {
+        val firstValue = Random.nextInt()
+        val secondValue = Random.nextInt()
+        val expectedValue = firstValue + secondValue
+        Truth.assertThat(sum(firstValue, secondValue)).isEqualTo(expectedValue)
     }
 }
