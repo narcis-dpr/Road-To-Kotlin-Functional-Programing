3b7c8e122650d88590f97423fdaa482b72f88472
n.dipir <nargesdeypir@gmail.com>
2024-03-17 09:55:30 +0330

    monoids and semigroups -- MonoidIntAdd

--- Monoids/src/main/java/com/narcis/monoids/Monoid.kt
+++ Monoids/src/main/java/com/narcis/monoids/Monoid.kt
@@ -4,3 +4,10 @@ interface Monoid<T> {
     val unit: T
     val combine:  T.(T) -> T   // also : (T) -> (T) -> T
 }
+
+object MonoidIntAdd: Monoid<Int> {
+    override val unit: Int
+        get() = 0
+    override val combine: Int.(Int) -> Int
+        get() = Int::plus
+}
