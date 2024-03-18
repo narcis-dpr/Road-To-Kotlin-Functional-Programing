23012f4dd924f1efb9cffe8d737f9065317475f1
n.dipir <nargesdeypir@gmail.com>
2024-03-18 12:46:10 +0330

    monoids and semigroups -- foldable : add iterator and test

--- Monoids/src/main/java/com/narcis/monoids/Foldable.kt
+++ Monoids/src/main/java/com/narcis/monoids/Foldable.kt
@@ -1,8 +1,18 @@
 package com.narcis.monoids
 
-
 fun List<Int>.sumList() = fold(0) {a,b -> a + b}
 
 fun String.reversString() = foldRight("") {char, str -> str + char}
 
+fun <T> Foldable<T>.fold(monoid: Monoid<T>): T =fold(monoid.unit, monoid.combine)
+
+fun main() {
+    listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).sumList() pipe ::println
+    "supercalifragilisticexpialidocious".reversString() pipe ::println
+}
+
+typealias Foldable<T> = Iterable<T>
+infix fun <A, B> A.pipe(f: Fun<A, B>): B = f(this)
+
+typealias Fun <A,B> = (A) -> B
 
