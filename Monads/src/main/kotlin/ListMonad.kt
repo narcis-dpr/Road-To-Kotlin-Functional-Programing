79a0b0164446c97640e86c691daf0543f89c08c0
narcis-dpr <nargesdeypir@gmail.com>
2024-03-20 13:32:51 +0330

    Monadas -- list monads

---
+++ Monads/src/main/kotlin/ListMonad.kt
@@ -0,0 +1,27 @@
+fun <T> List<List<T>>.listFlatten(): List<T> = this.fold(mutableListOf()) { acc, item ->
+    acc.apply {
+        addAll(item)
+    }
+}
+
+infix fun <B, C> List<B>.listBind(
+    g: Fun<B, List<C>>
+): List<C> = map(g).listFlatten()
+
+infix fun <A, B, C> Fun<A, List<B>>.listFish(
+    g: Fun<B, List<C>>
+): Fun<A, List<C>> = { a: A ->
+    this(a).listBind(g)
+}
+
+val countList: (Int) -> List<Int> = { n: Int -> List(n) { it + 1 } }
+
+val intToChars = {n: Int -> List(n) {'a' + n} }
+
+fun main() {
+    val fished = countList listFish intToChars
+    fished(3) pipe ::println
+
+}
+
+infix fun <A,B> A.pipe(f: Fun<A, B>): B = f(this)
