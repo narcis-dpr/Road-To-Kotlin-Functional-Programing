eb4760d8f8b63e7336a6ed06cc05325e2cdd49ee
narcis-dpr <nargesdeypir@gmail.com>
2024-03-22 17:10:17 +0330

    Error handling -- show search service : user data class validations

--- Error Handling/src/main/java/com/example/errorhandling/tVApp/app/src/main/java/com/example/android/validation/User.kt
+++ Error Handling/src/main/java/com/example/errorhandling/tVApp/app/src/main/java/com/example/android/validation/User.kt
@@ -1,7 +1,25 @@
 package com.example.errorhandling.tVApp.app.src.main.java.com.example.android.validation
 
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.Error
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.ResultAp
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.Success
+
 data class User(
     val id: Int,
     val name: String,
     val email: String
 )
+
+class ValidationException(msg: String) : Exception(msg)
+
+fun validateName(name: String): ResultAp<ValidationException, String> = if (name.length > 4) {
+    Success(name)
+} else {
+    Error(ValidationException("invalid name"))
+}
+
+fun validateEmail(email: String): ResultAp<ValidationException, String> = if (email.contains("@")) {
+    Success(email)
+} else {
+    Error(ValidationException("Invalid email"))
+}
