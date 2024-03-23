f86ff74dcb0ccae7c76dace242e994ff1bb2cde7
narcis-dpr <nargesdeypir@gmail.com>
2024-03-23 21:37:20 +0330

    Error handling -- show search service : apsg

--- Error Handling/src/main/java/com/example/errorhandling/tVApp/app/src/main/java/com/example/android/validation/ValidationSemiproup.kt
+++ Error Handling/src/main/java/com/example/errorhandling/tVApp/app/src/main/java/com/example/android/validation/ValidationSemiproup.kt
@@ -1,5 +1,10 @@
 package com.example.errorhandling.tVApp.app.src.main.java.com.example.android.validation
 
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.Error
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.ResultAp
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.Success
+import com.example.errorhandling.tVApp.app.src.main.java.com.example.android.applicative.successMap
+
 interface Semigroup<T> {
     operator fun plus(rh: T): T
 }
@@ -14,4 +19,14 @@ data class ValidationExceptionComposite(
         return errors.joinToString { it.localizedMessage }
     }
 
+}
+
+fun <E, T, R> ResultAp<E, T>.apsg(
+    fn: ResultAp<E, (T) -> R>
+): ResultAp<E, R> where E: Throwable, E: Semigroup<E> = when(fn) {
+    is Error -> when(this) {
+        is Error -> Error(this.error + fn.error)
+        is Success -> Error(fn.error)
+    }
+    is Success -> successMap(fn.value)
 }
