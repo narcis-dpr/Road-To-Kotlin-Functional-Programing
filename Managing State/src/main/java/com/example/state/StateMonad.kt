6d08b65436a39f99861635acf592ed152d564638
narcis-dpr <nargesdeypir@gmail.com>
2024-03-25 09:31:32 +0330

    managing state -- state monad : flat map

--- Managing State/src/main/java/com/example/state/StateMonad.kt
+++ Managing State/src/main/java/com/example/state/StateMonad.kt
@@ -1,3 +1,6 @@
 package com.example.state
 
-fun <S, A, B> State<S, A>.flatMap(fn: (A) -> State<S, B>): State<S, B> = TODO()
+fun <S, A, B> State<S, A>.flatMap(fn: (A) -> State<S, B>): State<S, B> = State { s0: S ->
+    val (a, s1) = this(s0)
+    fn(s1)(a)
+}
