3f8122d63855cdf0af60020ade8d6c144e93bc3d
narcis-dpr <nargesdeypir@gmail.com>
2024-03-25 20:15:03 +0330

    Handling side effect -- composing pure function for greetings

--- Handlig Side Effect/src/main/java/com/example/sideeffect/Greetings.kt
+++ Handlig Side Effect/src/main/java/com/example/sideeffect/Greetings.kt
@@ -7,14 +7,28 @@ val readName: (World) -> Pair<String, World> = { w: World ->
     Scanner(System.`in`).nextLine() to World
 }
 
+/**
+ * printString doesnt execute a side effect but returns a description of it ! :
+ */
 val printString: (String) -> SideEffect = { str: String ->
     { a: World ->
         print(str) to World
     }
 }
 
+fun askNameAndPrintGreetings(): (World) -> World = {w0: World ->
+    val w1 = printString("whats your name? ")(w0) // w1 is the new state of the world
+    val (name, w2) = readName(w1)
+    printString("Hello $name! \n")(w2)
+}
 fun main() {
     print("whats your name?")
     val name = Scanner(System.`in`).nextLine()
     print("Hello $name\n")
+
+    readName(World) pipe ::println
+    printString("Hello Narcis \n")(World) pipe ::println
+
+    // this way of composing functions is called monad comprehension :
+    askNameAndPrintGreetings()(World) pipe ::println
 }
