5b470be71aaa0a3b47305bb4a9dbfad4315b1653
narcis-dpr <nargesdeypir@gmail.com>
2024-03-25 19:51:51 +0330

    Handling side effect -- impure greetings

---
+++ Handlig Side Effect/src/main/java/com/example/sideeffect/Greetings.kt
@@ -0,0 +1,9 @@
+package com.example.sideeffect
+
+import java.util.Scanner
+
+fun main() {
+    print("whats your name?")
+    val name = Scanner(System.`in`).nextLine()
+    print("Hello $name\n")
+}
