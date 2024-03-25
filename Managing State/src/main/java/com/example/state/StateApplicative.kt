74b4387939712a2b9ef9220b29f4e82222e0c9ea
narcis-dpr <nargesdeypir@gmail.com>
2024-03-25 09:16:24 +0330

    managing state -- state applicative (without the test)

--- Managing State/src/main/java/com/example/state/StateApplicative.kt
+++ Managing State/src/main/java/com/example/state/StateApplicative.kt
@@ -8,4 +8,12 @@ fun replaceSuffix(
 
 val cReplaceSuffix = ::replaceSuffix.curry()
 
-//infix fun <S, A, B> State<S, (A) -> B>.appl(a: State<S, A>) = a.ap(this)
+fun <S, T, R> State<S, T>.ap(fn: State<S, (T) -> R>): State<S, R> = State { s0: S ->
+    val (t, s1) = this(s0)
+    val (fnValue, s2) = fn(t)
+    fnValue to s2(s1)
+}
+infix fun <S, A, B> State<S, (A) -> B>.appl(a: State<S, A>) = a.ap(this)
+fun main() {
+
+}
