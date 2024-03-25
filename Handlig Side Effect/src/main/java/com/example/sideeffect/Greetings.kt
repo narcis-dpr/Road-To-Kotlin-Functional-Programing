3ca208e74e5d070196ea4fa09ab7b35ba0c32109
narcis-dpr <nargesdeypir@gmail.com>
2024-03-25 20:01:51 +0330

    Handling side effect -- update greetings withoud sideEffect

--- Handlig Side Effect/src/main/java/com/example/sideeffect/Greetings.kt
+++ Handlig Side Effect/src/main/java/com/example/sideeffect/Greetings.kt
@@ -2,6 +2,17 @@ package com.example.sideeffect
 
 import java.util.Scanner
 
+
+val readName: (World) -> Pair<String, World> = { w: World ->
+    Scanner(System.`in`).nextLine() to World
+}
+
+val printString: (String) -> SideEffect = { str: String ->
+    { a: World ->
+        print(str) to World
+    }
+}
+
 fun main() {
     print("whats your name?")
     val name = Scanner(System.`in`).nextLine()
