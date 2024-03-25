baed078b1a0e4a324e916f8fcec95908c9ffae55
narcis-dpr <nargesdeypir@gmail.com>
2024-03-25 10:35:19 +0330

    managing state -- update utiles with FList

--- Managing State/src/main/java/com/example/state/Utiles.kt
+++ Managing State/src/main/java/com/example/state/Utiles.kt
@@ -41,3 +41,39 @@ fun <T1, T2, T3, T4, T5, R> Fun5<T1, T2, T3, T4, T5, R>.curry(): (T1) -> (T2) ->
             this(t1, t2, t3, t4, t5)
         }.curry()
     }
+
+
+sealed interface FList<out T> {
+
+    companion object {
+        @JvmStatic
+        fun <T> of(vararg items: T): FList<T> {
+            val tail = items.sliceArray(1 until items.size)
+            return if (items.isEmpty()) empty() else FCons(items[0], of(*tail))
+        }
+
+        @JvmStatic
+        fun <T> empty(): FList<T> = Nil
+    }
+}
+
+internal object Nil : FList<Nothing>
+internal data class FCons<T>(val head: T, val tail: FList<T> = Nil) : FList<T>
+
+fun <T, S> FList<T>.match(
+    whenNil: () -> S,
+    whenCons: (head: T, tail: FList<T>) -> S
+) =
+    when (this) {
+        is Nil -> whenNil()
+        is FCons<T> -> whenCons(head, tail)
+    }
+
+
+fun <T> FList<T>.forEach(fn: (T) -> Unit): Unit = match(
+    whenNil = {},
+    whenCons = { head, tail ->
+        fn(head)
+        tail.forEach(fn)
+    }
+)
