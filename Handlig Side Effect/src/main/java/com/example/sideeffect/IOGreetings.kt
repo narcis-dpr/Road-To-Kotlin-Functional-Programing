23a4f4a893c604336b5c96d0fbdc94cca955c5ba
narcis-dpr <nargesdeypir@gmail.com>
2024-03-26 17:02:57 +0330

    Handling side effect -- Io greetings : input output string

--- Handlig Side Effect/src/main/java/com/example/sideeffect/IOGreetings.kt
+++ Handlig Side Effect/src/main/java/com/example/sideeffect/IOGreetings.kt
@@ -4,3 +4,13 @@ val readNameM: IO<String> = IO(readNameT)
 val printStringM: (String) -> IO<Unit> = printStringT compose ::IO
 
 fun <T> IO<T>.bind(): T = this(World).first
+
+fun askNameAndPrintGreetingIO() : () -> Unit = {
+    printStringM("whats your name?").bind()
+    val name = readNameM.bind()
+    printStringM("Hello $name! \n").bind()
+}
+
+fun main() {
+    askNameAndPrintGreetingIO().invoke()
+}
