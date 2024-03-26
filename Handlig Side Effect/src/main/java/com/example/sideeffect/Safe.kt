cf3b8bfdc99f349cde6d273edce230673a0be91f
narcis-dpr <nargesdeypir@gmail.com>
2024-03-26 17:47:02 +0330

    Handling side effect -- test safe greetings

--- Handlig Side Effect/src/main/java/com/example/sideeffect/Safe.kt
+++ Handlig Side Effect/src/main/java/com/example/sideeffect/Safe.kt
@@ -17,3 +17,31 @@ val safeReadNameError: (World) -> Pair<Result<String>, World> = {w: World ->
 
 val safeReadNameT: WorldT<Result<String>> = safeReadName
 
+val safePrintStringT: (String) -> WorldT<Result<Unit>> = {str: String ->
+    {w: World ->
+        Result.success(Unit) to printString(str)(w)
+    }
+}
+
+/*
+    the monadic versions :
+ */
+
+val safeReadNameM: IO<Result<String>> = IO(safeReadNameT)
+
+val safePrintStringM: (String) -> IO<Result<Unit>> =
+    safePrintStringT compose ::IO
+
+
+fun safeAskNameAndPrintGreetingsIO(): () -> Result<Unit> = {
+    safePrintStringM("whats your name?").bind()
+    // .flatMap { _ -> safeReadNameM.bind() : ERROR ! flatMap not found
+    // flatMap { name -> safePrintStringM(Hello $name! \n).bind()
+}
+
+fun main() {
+    safeAskNameAndPrintGreetingsIO().invoke().fold(
+        onSuccess = { _ -> },
+        onFailure = {ex -> println(" Error: $ex ") }
+    )
+}
