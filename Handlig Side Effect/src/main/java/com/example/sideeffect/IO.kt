f0559cc06741cfeffb8661c6fb174d320cf20bfe
narcis-dpr <nargesdeypir@gmail.com>
2024-03-26 13:11:59 +0330

    Handling side effect -- Io ap and appl

--- Handlig Side Effect/src/main/java/com/example/sideeffect/IO.kt
+++ Handlig Side Effect/src/main/java/com/example/sideeffect/IO.kt
@@ -17,3 +17,13 @@ fun <A, B> IO<A>.map (fn: Fun<A, B>): IO<B> = IO {w0 ->
     val (a, w1) = this(w0)
     fn(a) to w1
 }
+
+fun <T, R> IO<T>.ap(
+    fn: IO<(T) -> R>
+): IO<R> = IO {w0: World ->
+    val (t, w1) = this(w0)
+    val (fnValue, w2) = fn(w1)
+    fnValue(t) to w2
+}
+
+infix fun <A, B> IO<(A) -> B>.appl(a: IO<A>) = a.ap(this)
