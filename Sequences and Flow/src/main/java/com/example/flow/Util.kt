d190402262e048140f9c2d81ac01d2bc8b74734d
narcis-dpr <nargesdeypir@gmail.com>
2024-03-26 19:35:53 +0330

    sequence and flow -- copy paste utiles

---
+++ Sequences and Flow/src/main/java/com/example/flow/Util.kt
@@ -0,0 +1,13 @@
+package com.example.flow
+
+fun Int.times(fn: (Int) -> Unit) = (1..this).forEach(fn)
+
+fun <A, B> ((A) -> B).logged(str: String): (A) -> B = {
+  val result = this(it)
+  println("$str($it) = $result ")
+  result
+}
+
+val double = { a: Int -> 2 * a }
+
+val filterOdd = { a: Int -> a % 2 == 0 }
